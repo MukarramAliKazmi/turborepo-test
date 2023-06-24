f717d5a12cf0c2bafb67926e7ab7f9713242f351
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-06-24 19:05:39 +0500

    fix(ui): Comment DisciplineCard component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -3,6 +3,6 @@ import '@/styles.css'
 
 // components
 export * from '@/components/Button'
-export * from '@/components/DisciplineCard'
+// export * from '@/components/DisciplineCard'
 // export * from '@/components/Input'
 // export * from '@/components/Layout'
