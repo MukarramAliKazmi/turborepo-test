95e168bacf00242f5744c7d49285e80f5568c254
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-06-24 17:07:47 +0500

    feat(eslint): Add custom eslint configuration

--- apps/docs/src/app/layout.tsx
+++ apps/docs/src/app/layout.tsx
@@ -1,15 +1,12 @@
+import React from "react";
 import "@/styles/globals.css";
 // include styles from the ui package
 import "ui/styles.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en" className="bg-zinc-900">
-      <body>{children}</body>
-    </html>
-  );
-}
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang="en" className="bg-zinc-900">
+    <body>{children}</body>
+  </html>
+);
+
+export default RootLayout;
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,64 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
+  env: {
+    browser: true,
+    node: true,
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+  extends: [
+    'next',
+    'turbo',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  settings: {
+    next: {
+      rootDir: ['apps/*/', 'packages/*/'],
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: ['apps/*/tsconfig.json', 'packages/*/tsconfig.json'],
+      },
     },
   },
-};
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+
+    'react/jsx-props-no-spreading': 'off',
+
+    '@next/next/no-html-link-for-pages': 'off',
+
+    'prettier/prettier': ['error'],
+
+    "import/no-extraneous-dependencies": 'off',
+
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['./**', '../**'],
+      },
+    ],
+  },
+  ignorePatterns: [
+    '**/*.js',
+    '**/*.json',
+    'node_modules',
+    'public',
+    'styles',
+    '.next',
+    'coverage',
+    'dist',
+    '.turbo',
+  ],
+}
