86684312d80b5c0dc24ca1fb672f5079538b5913
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-06-24 18:46:48 +0500

    fix(ui): Fix the name of the button component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,7 +2,7 @@
 import '@/styles.css'
 
 // components
-export * from '@/components/button'
+export * from '@/components/Button'
 // export * from '@/components/discipline-card'
 // export * from '@/components/input'
 // export * from '@/components/layout'
