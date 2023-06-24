2eb53025c3788e9c5d081eaac1c0f9b59a95605f
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-06-24 19:27:52 +0500

    fix(ui): Add Button component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,7 +2,7 @@
 import '@/styles.css'
 
 // components
-// export * from '@/components/Button'
+export * from '@/components/Button'
 // export * from '@/components/DisciplineCard'
 // export * from '@/components/Input'
 // export * from '@/components/Layout'
