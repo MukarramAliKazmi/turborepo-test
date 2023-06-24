f67e6d0c3580fa17f5fedf1fe82bbd596493c50f
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-06-24 18:44:19 +0500

    fix(ui): Export button component from ui

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,7 +2,7 @@
 import '@/styles.css'
 
 // components
-// export * from '@/components/button'
+export * from '@/components/button'
 // export * from '@/components/discipline-card'
 // export * from '@/components/input'
 // export * from '@/components/layout'
