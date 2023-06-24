7a832fd14f064ff605f74063120e6a8325afdcc8
Syed Mukarram Ali Kazmi <syed@fancity.co>
2023-06-24 20:28:07 +0500

    feat(ui): Add Topbar component

---
+++ packages/ui/src/components/Topbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ToggleSidebarButton } from '@/components/ToggleSidebarButton'
+import { ToggleThemeButton } from '@/components/ToggleThemeButton'
+
+export interface TopbarProps {
+  isSidebarOpen: boolean
+  handleIsSidebarOpen: () => void
+}
+
+export const Topbar: React.FC<TopbarProps> = ({
+  isSidebarOpen,
+  handleIsSidebarOpen,
+}) => (
+  <div className="w-full h-20 flex items-center justify-between px-4 md:px-6">
+    <div className="text-5xl font-bold text-sky-500">nustX</div>
+    <div className="flex items-center gap-4">
+      <ToggleThemeButton />
+      <ToggleSidebarButton
+        isOpen={isSidebarOpen}
+        handleIsOpen={handleIsSidebarOpen}
+      />
+    </div>
+  </div>
+)
